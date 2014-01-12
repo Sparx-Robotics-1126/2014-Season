09c8409bd9d6f6a9ccb5fe6119977c74f2182852
secretspy97 <nathan.connor@gosparx.org>
2014-01-12 14:23:01 -0500

    Stopped in the middle of adding exceptions to auto.

--- src/org/gosparx/Autonomous.java
+++ src/org/gosparx/Autonomous.java
@@ -16,30 +16,43 @@ public class Autonomous {
     /**************************************************************************/
     /*************************Manual Switch Voltages **************************/
     /**************************************************************************/
-    public static final double AUTO_SETTING_0 = 3.208;
-    public static final double AUTO_SETTING_1 = 3.126;
-    public static final double AUTO_SETTING_2 = 3.036;
-    public static final double AUTO_SETTING_3 = 2.935;
-    public static final double AUTO_SETTING_4 = 2.824;
-    public static final double AUTO_SETTING_5 = 2.701;
-    public static final double AUTO_SETTING_6 = 2.563;
-    public static final double AUTO_SETTING_7 = 2.405;
-    public static final double AUTO_SETTING_8 = 2.225;
+    private static final double AUTO_SETTING_0 = 3.208;
+    private static final double AUTO_SETTING_1 = 3.126;
+    private static final double AUTO_SETTING_2 = 3.036;
+    private static final double AUTO_SETTING_3 = 2.935;
+    private static final double AUTO_SETTING_4 = 2.824;
+    private static final double AUTO_SETTING_5 = 2.701;
+    private static final double AUTO_SETTING_6 = 2.563;
+    private static final double AUTO_SETTING_7 = 2.405;
+    private static final double AUTO_SETTING_8 = 2.225;
     /**************************************************************************/
     /************************ Autonomous commands *****************************/
     /**************************************************************************/
     
     /* Drives */
-    public static final int DRIVES_GO_FORWARD               = 1;//distance(inches), speed (int)
-    public static final int DRIVES_GO_REVERSE               = 2;
-    public static final int DRIVES_TURN_RIGHT               = 3;
-    public static final int DRIVES_TURN_LEFT                = 4;
-    public static final int DRIVES_STOP                     = 5;
-    public static final int DRIVES_DONE                     = 9;
+    private static final int DRIVES_GO_FORWARD              = 1;//distance(inches), speed(double)
+    private static final int DRIVES_GO_REVERSE              = 2;//distance(inches), speed(double)
+    private static final int DRIVES_TURN_RIGHT              = 3;
+    private static final int DRIVES_TURN_LEFT               = 4;
+    private static final int DRIVES_STOP                    = 5;
+    private static final int DRIVES_DONE                    = 6;
+    
+    /* Aquire */
+    private static final int INTAKE_AQUIRE_BALL             = 10;
+    private static final int INTAKE_REVERSE                 = 11;
+    private static final int INTAKE_IN_POSITION             = 12;
+    private static final int INTAKE_DONE                    = 13;
+    
+    /* Shooter */
+    private static final int SHOOTER_SHOOT                  = 20;
+    private static final int SHOOTER_SET_PRESET             = 21;
+    private static final int SHOOTER_IN_POSITION            = 22;
+    private static final int SHOOTER_DONE                   = 23;
 
     /* Misc */
-    public static final int WAIT                            = 90;
-    public static final int END                             = 99;
+    private static final int LOOP                           = 97;//number of loops, 
+    private static final int WAIT                           = 98;
+    private static final int END                            = 99;
     /**************************************************************************/
     /**************************** End of commands *****************************/
     /**************************************************************************/
@@ -81,44 +94,59 @@ public class Autonomous {
            }else{
                currentAutonomous = noAuto;
            }
-//        }
     }
     
     public void run(){
         getAutoMode();
         int start = 0, current = start, finished = currentAutonomous.length;
-//        print("Length of Array: " + finished);
         while (true){
-            while(getLocalMode() == SubSystem.AUTO &&  isEnabled()){
+            while(DriverStation.isAutonomous() &&  isEnabled()){
                     current++;
                 for (int i = start; i <= finished; i++){
                     if (isEnabled() && runAutonomous){
                     switch (currentAutonomous[i][0]){
                         case DRIVES_GO_FORWARD:
-                            print("Driving forward");
-                            break;
+                            throw new java.lang.RuntimeException("Drive Foward hasen't been created!!!");
+                            //break;
                         case DRIVES_GO_REVERSE:
-                            print("Driving reverse");
-                            break;
+                            throw new java.lang.RuntimeException("Drive Reverse hasen't been created!!!");
+                            //break;
                         case DRIVES_TURN_LEFT:
-                            print("Turning Left");
-                            break;
+                            throw new java.lang.RuntimeException("Drive Left hasen't been created!!!");
+                            //break;
                         case DRIVES_TURN_RIGHT:
                             print("Turning Right");
                         case DRIVES_STOP:
                             print("Stopping");
-                            break;
+                            //break;
                         case DRIVES_DONE:
                             print("DriveDone() was called");
-                            break;
-                            
+                            //break;
+                        case INTAKE_AQUIRE_BALL:
+                            //break;
+                        case INTAKE_REVERSE:
+                            //break;
+                        case INTAKE_IN_POSITION:
+                            //break;
+                        case INTAKE_DONE:
+                            //break;
+                        case SHOOTER_SHOOT:
+                            //break;
+                        case SHOOTER_SET_PRESET:
+                            //break;
+                        case SHOOTER_IN_POSITION:
+                            //break;
+                        case SHOOTER_DONE:
+                            //break;
+                        case LOOP:
+                            //break;
                         case WAIT:
-                            wait(currentAutonomous[i][1]);
-                            break;
+//                            wait(currentAutonomous[i][1]);
+                            //break;
                         case END:
                             runAutonomous = false;
                         default:
-                            print("No case statement: " + currentAutonomous[i]);
+//                            print("No case statement: " + currentAutonomous[i]);
                     }
                 } 
                 sleep(30);   
