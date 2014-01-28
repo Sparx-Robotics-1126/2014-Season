d53ac000da1d8a1c5ec85dc2fc5a325ddc5e8631
secretspy97 <nathan.connor@gosparx.org>
2014-01-27 20:06:14 -0500

    Removed a random /**

--- src/org/gosparx/subsystem/Drives.java
+++ src/org/gosparx/subsystem/Drives.java
@@ -142,7 +142,6 @@ public class Drives extends GenericSubsystem {
     }
     
     /**
-    /**
      * Creates a drives subsystem for controlling the drives subsystem.
      */
     public Drives(){
